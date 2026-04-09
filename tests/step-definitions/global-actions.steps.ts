45756f5b93e44ef2818e7132c011fefb04e96fdd
Ashok Kumar Dikshit <>
2026-04-09 02:54:59 +0100

    add function into global-actions.steps

--- tests/step-definitions/global-actions.steps.ts
+++ tests/step-definitions/global-actions.steps.ts
@@ -1,5 +1,6 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
-
+import { $ } from '@wdio/globals';
+const commonActions = require('../support/commonActions');
 // ===============================
 // GLOBAL ACTIONS STEP DEFINITIONS
 // ===============================
@@ -11,17 +12,17 @@ import { Given, When, Then } from '@wdio/cucumber-framework';
 // ===============================
 When(/^I click on (\w+) (button|link|element)$/, async (elementName: string, elementType: string) => {
     console.log(`Clicking on ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.clickOn(elementName, elementType);
 });
 
 When(/^I double click on (\w+) (button|link|element)$/, async (elementName: string, elementType: string) => {
     console.log(`Double clicking on ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.doubleClickOn(elementName, elementType);
 });
 
 When(/^I right click on (\w+) (button|link|element)$/, async (elementName: string, elementType: string) => {
     console.log(`Right clicking on ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.rightClickOn(elementName, elementType);
 });
 
 // ===============================
@@ -29,12 +30,12 @@ When(/^I right click on (\w+) (button|link|element)$/, async (elementName: strin
 // ===============================
 When(/^I select "([^"]*)" from (\w+) dropdown$/, async (option: string, elementName: string) => {
     console.log(`Selecting "${option}" from ${elementName} dropdown`);
-    // Implementation needed
+    await commonActions.selectFromDropdown(elementName, option);
 });
 
 When(/^I select (\w+) option from (\w+) dropdown$/, async (option: string, elementName: string) => {
     console.log(`Selecting ${option} option from ${elementName} dropdown`);
-    // Implementation needed
+    await commonActions.selectFromDropdown(elementName, option);
 });
 
 // ===============================
@@ -42,12 +43,12 @@ When(/^I select (\w+) option from (\w+) dropdown$/, async (option: string, eleme
 // ===============================
 When(/^I hover over (\w+) (button|link|element)$/, async (elementName: string, elementType: string) => {
     console.log(`Hovering over ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.hoverOn(elementName, elementType);
 });
 
 When(/^I focus on (\w+) (field|input|element)$/, async (elementName: string, elementType: string) => {
     console.log(`Focusing on ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.focusOn(elementName, elementType);
 });
 
 // ===============================
@@ -55,32 +56,32 @@ When(/^I focus on (\w+) (field|input|element)$/, async (elementName: string, ele
 // ===============================
 When(/^I scroll to (\w+) (element|section)$/, async (elementName: string, elementType: string) => {
     console.log(`Scrolling to ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.scrollTo(elementName, elementType);
 });
 
 When(/^I scroll to (\w+) and click$/, async (elementName: string) => {
     console.log(`Scrolling to ${elementName} and clicking`);
-    // Implementation needed
+    await commonActions.scrollToAndClick(elementName);
 });
 
 When(/^I scroll to (\w+) and double click$/, async (elementName: string) => {
     console.log(`Scrolling to ${elementName} and double clicking`);
-    // Implementation needed
+    await commonActions.scrollToAndDoubleClick(elementName);
 });
 
 When(/^I scroll to (\w+) and right click$/, async (elementName: string) => {
     console.log(`Scrolling to ${elementName} and right clicking`);
-    // Implementation needed
+    await commonActions.scrollToAndRightClick(elementName);
 });
 
 When(/^I scroll (left|right|up|down) to (\w+) and click on (\w+)$/, async (direction: string, targetElement: string, clickElement: string) => {
     console.log(`Scrolling ${direction} to ${targetElement} and clicking on ${clickElement}`);
-    // Implementation needed
+    await commonActions.scrollDirectionToAndClick(direction, targetElement, clickElement);
 });
 
 When(/^I scroll (left|right|up|down) by (\d+) pixels$/, async (direction: string, pixels: string) => {
     console.log(`Scrolling ${direction} by ${pixels} pixels`);
-    // Implementation needed
+    await commonActions.scrollByPixels(direction, pixels);
 });
 
 // ===============================
@@ -88,64 +89,64 @@ When(/^I scroll (left|right|up|down) by (\d+) pixels$/, async (direction: string
 // ===============================
 When(/^I press and hold (\w+) (element|button)$/, async (elementName: string, elementType: string) => {
     console.log(`Pressing and holding ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.pressAndHold(elementName, elementType);
 });
 
 When(/^I release the hold on (\w+) (element|button)$/, async (elementName: string, elementType: string) => {
     console.log(`Releasing hold on ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.releaseHold(elementName, elementType);
 });
 
 When(/^I long press on (\w+) (element|button)$/, async (elementName: string, elementType: string) => {
     console.log(`Long pressing on ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.longPress(elementName, elementType);
 });
 
 When(/^I swipe from (\w+) to (\w+)$/, async (fromElement: string, toElement: string) => {
     console.log(`Swiping from ${fromElement} to ${toElement}`);
-    // Implementation needed
+    await commonActions.swipeFromTo(fromElement, toElement);
 });
 
 When(/^I swipe (left|right|up|down) on (\w+)$/, async (direction: string, elementName: string) => {
     console.log(`Swiping ${direction} on ${elementName}`);
-    // Implementation needed
+    await commonActions.swipeDirection(direction, elementName);
 });
 
 When(/^I pinch in on (\w+)$/, async (elementName: string) => {
     console.log(`Pinching in on ${elementName}`);
-    // Implementation needed
+    await commonActions.pinchIn(elementName);
 });
 
 When(/^I pinch out on (\w+)$/, async (elementName: string) => {
     console.log(`Pinching out on ${elementName}`);
-    // Implementation needed
+    await commonActions.pinchOut(elementName);
 });
 
 When(/^I zoom in on (\w+)$/, async (elementName: string) => {
     console.log(`Zooming in on ${elementName}`);
-    // Implementation needed
+    await commonActions.zoomIn(elementName);
 });
 
 When(/^I zoom out on (\w+)$/, async (elementName: string) => {
     console.log(`Zooming out on ${elementName}`);
-    // Implementation needed
+    await commonActions.zoomOut(elementName);
 });
 // ===============================
 // TEXT INPUT ACTIONS
 // ===============================
 When(/^I (type|enter) "([^"]*)" into (\w+) (field|input|textbox)$/, async (action: string, text: string, elementName: string, elementType: string) => {
     console.log(`Typing/Entering "${text}" into ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.typeText(text, elementName, elementType);
 });
 
 When(/^I clear (\w+) (field|input|textbox)$/, async (elementName: string, elementType: string) => {
     console.log(`Clearing ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.clearField(elementName, elementType);
 });
 
 When(/^I select all text in (\w+) (field|input|textbox)$/, async (elementName: string, elementType: string) => {
     console.log(`Selecting all text in ${elementName} ${elementType}`);
-    // Implementation needed
+    await commonActions.selectAllText(elementName, elementType);
 });
 
 // ===============================
@@ -153,17 +154,17 @@ When(/^I select all text in (\w+) (field|input|textbox)$/, async (elementName: s
 // ===============================
 When(/^I check (\w+) checkbox$/, async (elementName: string) => {
     console.log(`Checking ${elementName} checkbox`);
-    // Implementation needed
+    await commonActions.checkCheckbox(elementName);
 });
 
 When(/^I uncheck (\w+) checkbox$/, async (elementName: string) => {
     console.log(`Unchecking ${elementName} checkbox`);
-    // Implementation needed
+    await commonActions.uncheckCheckbox(elementName);
 });
 
 When(/^I select (\w+) radio button$/, async (elementName: string) => {
     console.log(`Selecting ${elementName} radio button`);
-    // Implementation needed
+    await commonActions.selectRadioButton(elementName);
 });
 
 
@@ -173,42 +174,42 @@ When(/^I select (\w+) radio button$/, async (elementName: string) => {
 // ===============================
 When(/^I navigate to "([^"]*)"$/, async (url: string) => {
     console.log(`Navigating to "${url}"`);
-    // Implementation needed
+    await commonActions.navigateTo(url);
 });
 
 When(/^I go back$/, async () => {
     console.log('Going back in browser');
-    // Implementation needed
+    await commonActions.goBack();
 });
 
 When(/^I go forward$/, async () => {
     console.log('Going forward in browser');
-    // Implementation needed
+    await commonActions.goForward();
 });
 
 When(/^I refresh the page$/, async () => {
     console.log('Refreshing the page');
-    // Implementation needed
+    await commonActions.refreshPage();
 });
 
 When(/^I reload the page$/, async () => {
     console.log('Reloading the page');
-    // Implementation needed
+    await commonActions.reloadPage();
 });
 
 When(/^I switch to tab (\d+)$/, async (tabIndex: string) => {
     console.log(`Switching to tab ${tabIndex}`);
-    // Implementation needed
+    await commonActions.switchToTab(tabIndex);
 });
 
 When(/^I open new tab$/, async () => {
     console.log('Opening new tab');
-    // Implementation needed
+    await commonActions.openNewTab();
 });
 
 When(/^I close current tab$/, async () => {
     console.log('Closing current tab');
-    // Implementation needed
+    await commonActions.closeCurrentTab();
 });
 
 
@@ -218,32 +219,32 @@ When(/^I close current tab$/, async () => {
 // ===============================
 When(/^I wait for (\w+) to be visible$/, async (elementName: string) => {
     console.log(`Waiting for ${elementName} to be visible`);
-    // Implementation needed
+    await commonActions.waitForVisible(elementName);
 });
 
 When(/^I wait for (\w+) to disappear$/, async (elementName: string) => {
     console.log(`Waiting for ${elementName} to disappear`);
-    // Implementation needed
+    await commonActions.waitForDisappear(elementName);
 });
 
 When(/^I wait for (\w+) to be enabled$/, async (elementName: string) => {
     console.log(`Waiting for ${elementName} to be enabled`);
-    // Implementation needed
+    await commonActions.waitForEnabled(elementName);
 });
 
 When(/^I wait for (\w+) to be clickable$/, async (elementName: string) => {
     console.log(`Waiting for ${elementName} to be clickable`);
-    // Implementation needed
+    await commonActions.waitForClickable(elementName);
 });
 
 When(/^I wait (\d+) seconds$/, async (seconds: string) => {
     console.log(`Waiting ${seconds} seconds`);
-    // Implementation needed
+    await commonActions.waitSeconds(seconds);
 });
 
 When('I wait for page to load', async () => {
     console.log('Waiting for page to load');
-    // Implementation needed
+    await commonActions.waitForPageLoad();
 });
 
 // ===============================
@@ -251,12 +252,12 @@ When('I wait for page to load', async () => {
 // ===============================
 When(/^I upload file "([^"]*)" to (\w+)$/, async (filePath: string, elementName: string) => {
     console.log(`Uploading file "${filePath}" to ${elementName}`);
-    // Implementation needed
+    await commonActions.uploadFile(filePath, elementName);
 });
 
 When(/^I select file "([^"]*)" from file picker$/, async (filePath: string) => {
     console.log(`Selecting file "${filePath}" from file picker`);
-    // Implementation needed
+    await commonActions.selectFileFromPicker(filePath);
 });
 
 
@@ -266,27 +267,27 @@ When(/^I select file "([^"]*)" from file picker$/, async (filePath: string) => {
 // ===============================
 When(/^I accept the alert$/, async () => {
     console.log('Accepting the alert');
-    // Implementation needed
+    await commonActions.acceptAlert();
 });
 
 When(/^I dismiss the alert$/, async () => {
     console.log('Dismissing the alert');
-    // Implementation needed
+    await commonActions.dismissAlert();
 });
 
 When(/^I type "([^"]*)" in alert prompt$/, async (text: string) => {
     console.log(`Typing "${text}" in alert prompt`);
-    // Implementation needed
+    await commonActions.typeInAlert(text);
 });
 
 When(/^I close the modal$/, async () => {
     console.log('Closing the modal');
-    // Implementation needed
+    await commonActions.closeModal();
 });
 
 When(/^I click outside the modal$/, async () => {
     console.log('Clicking outside the modal');
-    // Implementation needed
+    await commonActions.clickOutsideModal();
 });
 
 
@@ -296,47 +297,47 @@ When(/^I click outside the modal$/, async () => {
 // ===============================
 When(/^I rotate device to (landscape|portrait)$/, async (orientation: string) => {
     console.log(`Rotating device to ${orientation}`);
-    // Implementation needed
+    await commonActions.rotateDevice(orientation);
 });
 
 When(/^I shake the device$/, async () => {
     console.log('Shaking the device');
-    // Implementation needed
+    await commonActions.shakeDevice();
 });
 
 When(/^I lock the device$/, async () => {
     console.log('Locking the device');
-    // Implementation needed
+    await commonActions.lockDevice();
 });
 
 When(/^I unlock the device$/, async () => {
     console.log('Unlocking the device');
-    // Implementation needed
+    await commonActions.unlockDevice();
 });
 
 When(/^I put app in background for (\d+) seconds$/, async (seconds: string) => {
     console.log(`Putting app in background for ${seconds} seconds`);
-    // Implementation needed
+    await commonActions.putAppInBackground(seconds);
 });
 
 When(/^I bring app to foreground$/, async () => {
     console.log('Bringing app to foreground');
-    // Implementation needed
+    await commonActions.bringAppToForeground();
 });
 
 When(/^I take a screenshot$/, async () => {
     console.log('Taking a screenshot');
-    // Implementation needed
+    await commonActions.takeScreenshot();
 });
 
 When(/^I hide keyboard$/, async () => {
     console.log('Hiding keyboard');
-    // Implementation needed
+    await commonActions.hideKeyboard();
 });
 
 When(/^I show keyboard$/, async () => {
     console.log('Showing keyboard');
-    // Implementation needed
+    await commonActions.showKeyboard();
 });
 
 // ===============================
@@ -344,17 +345,17 @@ When(/^I show keyboard$/, async () => {
 // ===============================
 When(/^I submit the form$/, async () => {
     console.log('Submitting the form');
-    // Implementation needed
+    await commonActions.submitForm();
 });
 
 When(/^I reset the form$/, async () => {
     console.log('Resetting the form');
-    // Implementation needed
+    await commonActions.resetForm();
 });
 
 When(/^I fill form with:$/, async (dataTable: any) => {
     console.log('Filling form with data table:', dataTable.raw());
-    // Implementation needed for data table
+    await commonActions.fillFormWithData(dataTable);
 });
 
 
@@ -364,12 +365,12 @@ When(/^I fill form with:$/, async (dataTable: any) => {
 // ===============================
 When(/^I press (Enter|Tab|Escape|Space|Backspace|Delete) key$/, async (key: string) => {
     console.log(`Pressing ${key} key`);
-    // Implementation needed
+    await commonActions.pressKey(key);
 });
 
 When(/^I press key combination "([^"]*)"$/, async (keys: string) => {
     console.log(`Pressing key combination "${keys}"`);
-    // Implementation needed
+    await commonActions.pressKeyCombination(keys);
 });
 
 
@@ -379,11 +380,11 @@ When(/^I press key combination "([^"]*)"$/, async (keys: string) => {
 // ===============================
 When(/^I drag (\w+) to (\w+)$/, async (sourceElement: string, targetElement: string) => {
     console.log(`Dragging ${sourceElement} to ${targetElement}`);
-    // Implementation needed
+    await commonActions.dragToElement(sourceElement, targetElement);
 });
 
 When(/^I drag (\w+) by offset \((\d+),(\d+)\)$/, async (elementName: string, x: string, y: string) => {
     console.log(`Dragging ${elementName} by offset (${x},${y})`);
-    // Implementation needed
+    await commonActions.dragByOffset(elementName, x, y);
 });
 
